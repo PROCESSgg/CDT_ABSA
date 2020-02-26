864ca27d0d79da826af80a170d5a32ea391df294
PROCESSgg <nowlxd@nbjl.nankai.edu.cn>
2020-02-26 19:19:32 +0800

    学习了加载glove词向量到列表的方法

--- load_w2v.py
+++ load_w2v.py
@@ -7,6 +7,7 @@ import linecache
 import numpy as np
 
 # load pretrained word emb
+#按照word_list中的单词，从glove中读取词向量，得到word_list顺序的所有词向量，glove中没有的就用0向量初始化。
 def load_pretrained_embedding(glove_dir, word_list, dimension_size=300, encoding='utf-8'):
     pre_words = []
     count = 0
@@ -19,8 +20,11 @@ def load_pretrained_embedding(glove_dir, word_list, dimension_size=300, encoding
     word_vectors = []
     for word in word_list:
         if word in word2offset:
+            #从名为第一个参数的文件中得到第第二个参数行。这个函数从不会抛出一个异常–产生错误时它将返回”（换行符将包含在找到的行里）。如果文件没有找到，这个函数将会在sys.path搜索。
             line = linecache.getline(glove_dir+"/glove.840B.300d.txt", word2offset[word]+1)
+            #strip函数，默认参数是去掉字符串头尾的换行和空白格，判断要找的单词和词向量首部单词是否对号
             assert(word == line[:line.find(' ')].strip())
+            #np.fromstring函数是从字符串中按照sep=指定的分隔符转化到narray数组中。
             word_vectors.append(np.fromstring(line[line.find(' '):].strip(), sep=' ', dtype=np.float32))
             count += 1
         else:
